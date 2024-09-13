c87f52bca53995d79d526307a156cf08be3a57a6
Thomas Harvey <thomasharvey1415@gmail.com>
2024-09-13 11:43:18 +0100

    set up the layout of the website for desktop. Shop has not been designed yet

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 Week 3 project - Build a cookie clicker
+
+#TODO: make size of button scale to the size of browser AND make it not overflow from the button
+
+Resources:
+https://cssgrid-generator.netlify.app/ helped with creating the exact grid I want.
---
+++ app.js
@@ -0,0 +1,23 @@
+const resetButton = document.getElementById("reset-button");
+const cookie = document.getElementById("cookie");
+
+let cookieStats = {
+  cookieCount: 0,
+  cookiePerSecond: 0,
+};
+
+//TODO SHOP UPGRADES
+let shopUpgrades = [];
+
+//TODO GET DATA FROM API
+async function getShopItem() {
+  //fetch the data
+  //translate to JSON
+  //PUSH items into shopUpgrade arrays
+}
+
+//TODO event listener for all buttons
+
+//TODO timer to add CPS every second.
+
+//TODO Store values of cookie count and all upgrades in local storage
