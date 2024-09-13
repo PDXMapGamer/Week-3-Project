27df35a59feb80cff0c2e5de754467a2012d05bd
Thomas Harvey <thomasharvey1415@gmail.com>
2024-09-13 14:24:18 +0100

    Added functionality to the cookie and Cookie per Second calculation and event listeners to the other buttons

--- README.md
+++ README.md
@@ -2,5 +2,6 @@ Week 3 project - Build a cookie clicker
 
 #TODO: make size of button scale to the size of browser AND make it not overflow from the button
 
+Bug 1: setInterval only execute once. Solution: wrote =+ (= + after prettier autoformat) which set total cookies to be equal to CPS instead of incrementally adding it on
 Resources:
 https://cssgrid-generator.netlify.app/ helped with creating the exact grid I want.
--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 const resetButton = document.getElementById("reset-button");
+const menuButton = document.getElementById("menu-button");
+const displayCookieCount = document.getElementById("total-cookies");
+const displayCPS = document.getElementById("cookies-per-second");
 const cookie = document.getElementById("cookie");
 
 let cookieStats = {
@@ -9,6 +12,21 @@ let cookieStats = {
 //TODO SHOP UPGRADES
 let shopUpgrades = [];
 
+onStartUp(); // everything that needs to be done on start up of the page
+
+function onStartUp() {
+  updateCookies();
+  updateCPS();
+  initialiseTimer();
+}
+
+function updateCookies() {
+  displayCookieCount.textContent = `Total Cookies: ${cookieStats.cookieCount}`;
+}
+function updateCPS() {
+  displayCPS.textContent = `Cookies per Second: ${cookieStats.cookiePerSecond}`;
+}
+
 //TODO GET DATA FROM API
 async function getShopItem() {
   //fetch the data
@@ -17,7 +35,25 @@ async function getShopItem() {
 }
 
 //TODO event listener for all buttons
+cookie.addEventListener("click", function () {
+  cookieStats.cookieCount += 1;
+  updateCookies();
+  //TODO Local storage shenanigans
+});
+menuButton.addEventListener("click", function () {
+  console.log("Menu button has been clicked");
+  //TODO STREACH GOAL: Add functionality to this menu option
+});
+resetButton.addEventListener("click", function () {
+  //TODO Add functionality to the reset button
+});
 
 //TODO timer to add CPS every second.
+function initialiseTimer() {
+  setInterval(() => {
+    cookieStats.cookieCount += cookieStats.cookiePerSecond;
+    updateCookies();
+  }, 1000);
+}
 
 //TODO Store values of cookie count and all upgrades in local storage
